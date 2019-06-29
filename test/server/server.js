08d9d273902a9093d576fb9d9827448015029653
Simon <simoninreilly@gmail.com>
2019-06-29 19:43:54 +0100

    Add a mocking server for the test suite

---
+++ test/server/server.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express().use(bodyParser.json()); // creates http server
+const token = 'test'; // type here your verification token
+
+app.get('/', (req, res) => {
+    // check if verification token is correct
+    if (req.query.token !== token) {
+        return res.sendStatus(401);
+    }
+
+    // return challenge
+    return res.end(req.query.challenge);
+});
+
+app.post('/', (req, res) => {
+    // check if verification token is correct
+    if (req.query.token !== token) {
+        return res.sendStatus(401);
+    }
+
+    // print request body
+    console.log(req.body);
+
+    // return a text response
+    const data = {
+        responses: [
+            {
+                type: 'text',
+                elements: ['Hi', 'Hello']
+            }
+        ]
+    };
+
+    res.json(data);
+});
+
+app.listen(3000, () => console.log('[BotEngine] Webhook is listening'));
